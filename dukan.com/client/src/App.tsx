8395fd4f9e03b2ade90d1fc9e70f4b22c46b0a9f
Usman Ali Khan <ualikhan521@gmail.com>
2024-12-12 16:09:40 +0500

    React-Ecommerce-Project

---
+++ dukan.com/client/src/App.tsx
@@ -0,0 +1,14 @@
+import Container from "./ui/Container"
+
+function App() { 
+  return (
+    <main>
+      <Container>
+        <p>Ecommerce Demo</p>
+      </Container>
+    </main>
+    
+  )
+}
+
+export default App
